e4cba3efffcbc189f91d06755832565034587ca9
Camila M Reno <camilamreno@gmail.com>
2023-07-01 21:24:46 -0300

    feat: :sparkles: Adicionando a tela de edição

---
+++ src/screens/Edition.tsx
@@ -0,0 +1,23 @@
+import { KeyboardAvoidingView, Platform, SafeAreaView } from "react-native";
+import { Header } from "../components/Header";
+import AddDevices from "../components/AddDevices";
+import { ScrollView, View } from "native-base";
+import { HeaderSimple } from "../components/HeaderSimple";
+
+export default function Add() {
+  const scrollViewHeight = Platform.OS === "ios" ? "100%" : "80";
+  return (
+    <>
+      <HeaderSimple title={"Editar novos dispositivos"}/>
+      <KeyboardAvoidingView behavior={"height"}>
+        <SafeAreaView>
+          <ScrollView p={3}>
+            <View bgColor={"transparent"}>
+              <AddDevices />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
+    </>
+  );
+}
